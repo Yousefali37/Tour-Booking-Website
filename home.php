870672ea9fddc6b6f7b9dd5b52b6a981c7568c89
Yousef Ali <yousef.kobe24@gmail.com>
2024-05-30 19:31:41 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- font Awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- Css File Link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <section class="header">
+        <a href="home.php" class="logo">Take Me There.</a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+        <!--Home Section Started-->
+<section class="carousel">
+        <div class="list">
+            <div class="item">
+                <img src="images/bck-27.jpg">
+                <div class="content">
+                    <div class="author"></div>
+                    <div class="title"></div>
+                    <div class="topic">Air Balloon</div>
+                    <div class="des">
+                        Luxor offers incredible hot air balloon rides. These early morning adventures drift you over the ancient city, letting you admire the temples of Karnak and Luxor, the Colossi of Memnon statues, and even the Valley of the Kings from a unique bird's-eye view. It's a peaceful and unforgettable way to experience Luxor's history and beauty.
+                    </div>
+                    <a href="package.php" class="btn">SEE MORE</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/bck-36.jpg">
+                <div class="content">
+                    <div class="author"></div>
+                    <div class="title"></div>
+                    <div class="topic">Temples</div>
+                    <div class="des">
+                        Imagine a grand building honoring Egyptian gods. Picture giant statues, a forest of columns, and hieroglyphics everywhere. Built by pharaohs like Tutankhamun!
+                    </div>
+                    <a href="package.php" class="btn">SEE MORE</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/bck-47.jpg">
+                <div class="content">
+                    <div class="author"></div>
+                    <div class="title"></div>
+                    <div class="topic">Nile Rides</div>
+                    <div class="des">
+                    Luxor sunsets are famous for their fiery
+                    <br> colors - reds, oranges, purples - painting the sky 
+                    <br>as the sun sets over the Nile. Many enjoy
+                    <br> this view on a relaxing felucca ride.
+                    </div>
+                    <a href="package.php" class="btn">SEE MORE</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/bck-40.jpg">
+                <div class="content">
+                    <div class="author"></div>
+                    <div class="title"></div>
+                    <div class="topic">Mountain ranges</div>
+                    <div class="des">
+                    The Theban mountains provide a dramatic backdrop to the west bank. These dry, rocky mountains offer scenic views, especially during sunrise and sunset.
+                    </div>
+                    <a href="package.php" class="btn">SEE MORE</a>
+                </div>
+            </div>
+        </div>
+        <!-- list thumnail -->
+        <div class="thumbnail">
+            <div class="item">
+            <img src="images/bck-27.jpg">
+                <div class="content">
+                    <div class="title">
+                        air-ballon
+                    </div><div class="description">
+
+                </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/bck-36.jpg">
+                <div class="content">
+                    <div class="title">
+                        Temples
+                    </div>
+                    <div class="description"></div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/bck-47.jpg">
+                <div class="content">
+                    <div class="title">
+                        nile-rides
+                    </div>
+                    <div class="description"></div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/bck-40.jpg">
+                <div class="content">
+                    <div class="title">
+                    Mountain ranges
+                    </div>
+                    <div class="description">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- next prev -->
+
+        <div class="arrows">
+            <button id="prev"><</button>
+            <button id="next">></button>
+        </div>
+</section>
+<!--Home Section Ends-->
+<!-- Services Section Starts -->
+    <section class="services">
+            <h1 class="heading-title"> Our Services </h1>
+            <div class="box-container">
+                <div class="box">
+                    <img src="images/icon_1.svg" alt="">
+                    <h3>Crusing</h3>
+                </div>
+                <div class="box">
+                    <img src="images/icon_2.svg" alt="">
+                    <h3>Tour Guiding</h3>
+                </div>
+                <div class="box">
+                    <img src="images/icon_3.svg" alt="">
+                    <h3>Booking</h3>
+                </div>
+                <div class="box">
+                    <img src="images/icon_4.svg" alt="">
+                    <h3>Camping</h3>
+                </div>
+                <div class="box">
+                    <img src="images/icon_5.svg" alt="">
+                    <h3>Hot Air Balloon</h3>
+                </div>
+            </div>
+    </section>
+<!-- Services Section Ends -->
+<!--Home About Section Starts-->
+    <section class="home-about">
+            <div class="image">
+                <img src="images/bck-16.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>
+                    About Us
+                </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, amet sint eligendi assumenda animi quas, doloribus commodi similique fuga fugiat sequi aperiam, repudiandae eius nemo quae! Dolorum beatae illo accusantium?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit suscipit dignissimos aut numquam sint dicta similique eligendi praesentium ipsum id! Voluptatibus doloremque eaque veritatis sequi sunt beatae fugit quo mollitia!
+                    Nostrum autem distinctio eligendi. Sed at explicabo, alias obcaecati assumenda distinctio veniam deleniti impedit. Aliquam obcaecati ab, et quo corporis dolore earum!
+                </p>
+                <a href="about.php" class="btn">Read More</a>
+            </div>
+        </section>
+<!--Home About Section Ends-->
+<!--Home Packages Section Starts-->
+<section class="home-package">
+    <h1 class="heading-title">Our Packages</h1>
+    <div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="images/bck-21.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventures & Tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <a href="book.php" class="btn">Book Now</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bck-12.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventures & Tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <a href="book.php" class="btn">Book Now</a>
+            </div>
+        </div>
+            <div class="box">
+            <div class="image">
+                <img src="images/bck-30.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>Adventures & Tour</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <a href="book.php" class="btn">Book Now</a>
+            </div>
+        </div>
+    </div>
+    <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+</section>
+<!--Home About Section Ends-->
+<!--Home Offer Section Starts-->
+    <section class="home-offer">
+        <div class="content">
+            <h3>Upto 50% off</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum error doloremque quos velit labore fugit perferendis sed debitis, non ducimus? Veritatis aut perferendis repellendus eaque inventore voluptate officia dicta quae?</p>
+            <a href="book.php" class="btn">Book Now</a>
+        </div>
+    </section>
+
+<!--Home Offer Section Ends-->
+<!--Footer Strats-->
+    <section class="footer">
+
+    <div class="box-container">
+        <div class="box">
+            <h3>quick Links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+        <div class="box">
+            <h3>Extra Links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+        <div class="box">
+            <h3>Contact Info</h3>
+            <a href="#"><i class="fas fa-phone"></i> 000-0000-0000</a>
+            <a href="#"><i class="fas fa-phone"></i> 000-0000-0000</a>
+            <a href="#"><i class="fas fa-envelope"></i> TakeMeThere@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> Luxor, Egypt</a>
+        </div>
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i> Linkedin</a>
+        </div>
+    </div>
+
+
+    <div class="credit">Created by <span> El3rb </span> | all rights reserved! </div>
+
+    </section>
+<!--Footer Ends-->
+    
+    <!-- custome js file link -->
+    <script src="script.js"></script>
+</body>
+</html>
